06a1ffab780534bc0fa9ab37b31451ea970cce14
hyx-zzy <1911301363@qq.com>
2025-09-10 14:35:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# 在这个文件下编写代码，题目具体要求见README.md文件
+#input_str=input()
+input_str=input_str.strip()
+if input_str.endswith('kg'):
+  kg=float(input_str[:-2])
+  pd=kg*2.2046
+  print(f"对应的英制重量为{pd:.3f}磅")
+elif input_str.endswith('pd'):
+  pd=float(input_str[:-2])
+  kg=pd/2.2046
+  print(f"对应的公制重量为{kg:.3f}公斤")
+else:
+  print("输入格式错误,请输入如'10kg'或'10pd'的格式)
