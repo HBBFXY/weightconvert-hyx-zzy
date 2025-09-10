5cc95f6fea36a3d50fa4ef50f0c58ce1a7e72590
hyx-zzy <1911301363@qq.com>
2025-09-10 14:46:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#input_str=input()
-input_str=input_str.strip()
-if input_str.endswith('kg'):
-  kg=float(input_str[:-2])
+#WeightConvert.py
+WeightStr=input()
+if WeightStr[-2:]=='kg':
+  kg=float(WeightStr[:-2])
   pd=kg*2.2046
   print(f"对应的英制重量为{pd:.3f}磅")
-elif input_str.endswith('pd'):
-  pd=float(input_str[:-2])
+elif WeightStr[-2:]=='pd':
+  pd=float(WeightStr[:-2])
   kg=pd/2.2046
   print(f"对应的公制重量为{kg:.3f}公斤")
 else:
