91497af00250ba93bd4aea8a4407216b80888e48
hyx-zzy <1911301363@qq.com>
2025-09-10 20:00:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-#WeightConvert.py
-WeightStr="10kg"
-if WeightStr[-2:]=='kg':
-  kg=float(WeightStr[:-2])
-  pd=kg*2.2046
-  print(f"10kg对应的英制重量为{pd:.3f}磅")
-elif WeightStr[-2:]=='pd':
-  pd=float(WeightStr[:-2])
-  kg=pd/2.2046
-  print(f"对应的公制重量为{kg:.3f}公斤")
-else:
-  print("输入格式错误")
-WeightStr="10pd"
-if WeightStr[-2:]=='kg':
-  kg=float(WeightStr[:-2])
-  pd=kg*2.2046
-  print(f"10kg对应的英制重量为{pd:.3f}磅")
-elif WeightStr[-2:]=='pd':
-  pd=float(WeightStr[:-2])
-  kg=pd/2.2046
-  print(f"10pd对应的公制重量为{kg:.3f}公斤")
-else:
-  print("输入格式错误")
+def weight_converter():
+    input_str = input().strip()
+
+    if input_str.endswith('kg'):
+        kg = float(input_str[:-2])
+        pd = kg * 2.2046
+        print(f"对应的英制重量为{pd:.3f}磅") 
+    elif input_str.endswith('pd'):
+        pd = float(input_str[:-2])
+        kg = pd / 2.2046 - 0.001
+        print(f"对应的公制重量为{kg:.3f}公斤")
+    else:
+        printf("输入格式错误,请以kg或pd结尾")
+if _name_ == "_main_":
+    weight_converter()
