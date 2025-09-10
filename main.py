8aea6a6850accc1c8785e13acf52de451a4ae281
hyx-zzy <1911301363@qq.com>
2025-09-10 14:58:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 #WeightConvert.py
-WeightStr=input()
+WeightStr="10kg"
 if WeightStr[-2:]=='kg':
   kg=float(WeightStr[:-2])
   pd=kg*2.2046
-  print(f"对应的英制重量为{pd:.3f}磅")
+  print(f"10kg对应的英制重量为{pd:.3f}磅")
 elif WeightStr[-2:]=='pd':
   pd=float(WeightStr[:-2])
   kg=pd/2.2046
   print(f"对应的公制重量为{kg:.3f}公斤")
+else:
+  print("输入格式错误")
+WeightStr="10pd"
+if WeightStr[-2:]=='kg':
+  kg=float(WeightStr[:-2])
+  pd=kg*2.2046
+  print(f"10kg对应的英制重量为{pd:.3f}磅")
+elif WeightStr[-2:]=='pd':
+  pd=float(WeightStr[:-2])
+  kg=pd/2.2046
+  print(f"10pd对应的公制重量为{kg:.3f}公斤")
 else:
   print("输入格式错误")
